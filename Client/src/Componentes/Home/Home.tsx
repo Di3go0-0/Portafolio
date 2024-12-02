746094f00ec30deae95dd125472a582df1cdf016
di3go0-0 <d.rincon@utp.edu.co>
2024-12-02 00:34:56 -0500

    About finished

--- Client/src/Componentes/Home/Home.tsx
+++ Client/src/Componentes/Home/Home.tsx
@@ -1,8 +1,28 @@
+import { act } from 'react';
+import { Button } from '../Button/Button';
 import './Home.css'
 
 
+
+
 export const Home = () => {
+
+  const openPage = (url: string) => {
+    window.open(url, '_blank');
+  }
+
+  const downloadCV = () => {
+    const link = document.createElement('a');
+    link.href = '../../../Resources/IMG/Main.jpg';
+    link.download = 'CV_Diego_Rincon.jpg';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+
   return (
+
     <div className='Home'>
       <section className="image-section">
 
@@ -18,14 +38,32 @@ export const Home = () => {
             <h1>Diego Rincón</h1>
             <h5 className="text1">I'm a <span className="text2">Full Stack Developer</span></h5>
 
-            <p>I am a dedicated software developer passionate about crafting creative and efficient solutions. While I am at the beginning of my professional journey, I excel at quickly adapting to new technologies and consistently strive to deliver exceptional results.            </p>
+            <p>I am a dedicated software developer passionate about crafting creative
+              and efficient solutions. While I am at the beginning of my professional
+              journey, I excel at quickly adapting to new technologies and consistently
+              strive to deliver exceptional results.</p>
+
+            <div className='buttons'>
+              <Button parentMethod={() => openPage("https://www.linkedin.com/in/di3go00/")}>
+                Contact Me
+              </Button>
+              <Button parentMethod={downloadCV}>
+                Download CV
+              </Button>
+
+            </div>
           </div>
+
+
           <div className="img-container">
             <img src="../../../Resources/IMG/download.gif" alt="Gif" />
           </div>
 
         </div>
       </section >
+
+
+
     </div >
   );
 }
