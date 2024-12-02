6581d2bec075a0bc6d350def71154526df04a05c
di3go0-0 <d.rincon@utp.edu.co>
2024-12-01 23:48:22 -0500

    update about
    I need to add the cutton of contact

--- Client/src/Componentes/Home/Home.tsx
+++ Client/src/Componentes/Home/Home.tsx
@@ -11,6 +11,21 @@ export const Home = () => {
           <h1 className="efect2"><span className="color-h1">WELCOME</span> TO MY PORTAFOLIO</h1>
         </div>
       </section>
-    </div>
+
+      <section className='About-container'>
+        <div className='About'>
+          <div className="text-container">
+            <h1>Diego Rincón</h1>
+            <h5 className="text1">I'm a <span className="text2">Full Stack Developer</span></h5>
+
+            <p>I am a dedicated software developer passionate about crafting creative and efficient solutions. While I am at the beginning of my professional journey, I excel at quickly adapting to new technologies and consistently strive to deliver exceptional results.            </p>
+          </div>
+          <div className="img-container">
+            <img src="../../../Resources/IMG/download.gif" alt="Gif" />
+          </div>
+
+        </div>
+      </section >
+    </div >
   );
 }
